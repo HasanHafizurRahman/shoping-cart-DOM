7086c3decd431408752eab762dcfe390ed11d0b9
HasanHafizurRahman <hasanshanto922@gmail.com>
2022-02-14 00:57:25 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+function updateProductNumber(product, price, isIncreasing){
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if(isIncreasing){
+        productNumber = parseInt(productNumber) + 1;
+    }
+    else if(productNumber > 0){
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+    // update Total 
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+    // calculate total
+    calculateTotal();
+}
+   
+function getInputValue(product){
+    const productInput = document.getElementById(product + '-number').value;
+    const productNumber = parseInt(productInput);
+    return productNumber;
+}
+
+function calculateTotal(){
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    console.log(subTotal);
+}
+
+
+// handling phone increase decrease
+document.getElementById('phone-plus').addEventListener('click', function(){
+    updateProductNumber('phone', 1219, true);
+});
+document.getElementById('phone-minus').addEventListener('click', function(){
+    updateProductNumber('phone', 1219, false);
+})
+
+
+
+
+
+// handling case increase decrease
+document.getElementById('case-plus').addEventListener('click', function(){
+    updateProductNumber('case', 59, true);
+    
+})
+
+document.getElementById('case-minus').addEventListener('click', function(){
+updateProductNumber('case', 59, false);
+    
+})
+
+
+
+
+// WATCHED VIDEO OF 9:28 , MODULE 28-4;
